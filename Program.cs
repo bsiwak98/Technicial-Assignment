443e3ccafc048f8a2887c3eae7826c6f0fe7d8da
bsiwak98 <86394945+bsiwak98@users.noreply.github.com>
2021-07-13 05:24:27 -0400

    refactored

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿/*      
     Bibek Siwakoti
     06/24/2021
-    Techincal Assignment for GDC
+    Techincal Assignment Refactoring  for GDC
 */
 using System;
 using System.IO;
 using System.Collections;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
 namespace testApp
 {
     class Program
@@ -22,7 +24,7 @@ namespace testApp
                 ArrayList valid = new ArrayList();
                 ArrayList invalid = new ArrayList();
                 //split text into lines
-                foreach(string line in splittext)
+               foreach(string line in splittext)
                 {
                     //split lines into values
                     string[] splitline = line.Split(',');
@@ -30,7 +32,9 @@ namespace testApp
                     {   
                         //assume third value is email
                         string email = splitline[2];
-                        if(email.Contains('@') && email.Contains('.')) 
+                        Regex regex = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([azA-Z]{2,4}|[0-9]{1,3})(\]?)$");  
+                        Match match = regex.Match(email);  
+                        if(match.Success) 
                         {
                             valid.Add(email);                       
                         }
@@ -40,7 +44,7 @@ namespace testApp
                         }
                     }                   
 
-                }
+                } 
                 Console.WriteLine("Valid Emails");              
                 foreach(string email in valid) 
                 {
